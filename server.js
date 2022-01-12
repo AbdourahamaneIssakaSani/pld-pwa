db7e03ce2ecb387bc1815ae15243641664f21374
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2022-01-12 17:30:50 +0100

    fix versions - main and footer

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+//Install express server
 const express = require("express");
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/pld-pwa"));
 
-app.all("*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist/index.html");
-});
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/pld-pwa/" })
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
