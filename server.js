d497e0abc6c38a7f52308c72ac5d0d328938aec4
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2022-01-12 19:55:59 +0100

    fix: build issues on heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 
+const app = express();
 // app.use(express.static(__dirname + "/dist/pld-pwa"));
 
 // app.all("*", (req, res) => {
 //   res.status(200).sendFile(__dirname + "/dist/pld-pwa/index.html");
 // });
 
-app.use(express.static(__dirname + "/dist/pld-pwa"));
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "dist/pld-pwa/index.html"));
+app.use(express.static("dist/pld-pwa"));
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "dist/pld-pwa", "index.html"));
 });
 
 port = 8080;
