8928ba75c9f50813b71f5d6c448a419aa0413612
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2022-01-12 19:44:01 +0100

    fix: build issues on heroku

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const path = require("path");
 // });
 
 app.use(express.static(__dirname + "/dist/pld-pwa"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/pld-pwa/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "dist/pld-pwa/index.html"));
 });
 
 port = 8080;
